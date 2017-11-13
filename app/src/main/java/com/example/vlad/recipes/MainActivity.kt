5c997363e21fa70a157ad5ea80321a47d0783b91
Vlad <vlad_z88@mail.ru>
2017-11-13 10:45:01 +0300

    second commit) Trying to use sqlite

--- app/src/main/java/com/example/vlad/recipes/MainActivity.kt
+++ app/src/main/java/com/example/vlad/recipes/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.example.vlad.recipes
 
 import android.content.Intent
+import android.database.sqlite.SQLiteOpenHelper
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.vlad.recipes.database.RecipesDatabaseHelper
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            val dbHelper: SQLiteOpenHelper = RecipesDatabaseHelper(this)
+            val db = dbHelper.writableDatabase
+
+            val cursor = db.query(false, "products_types", null, null, null, null, null, null, null)
+
+//            cursor.moveToFirst()
+            println(cursor.count)
+        } catch(ex: Exception){
+            val toast: Toast = Toast.makeText(this, "Database is unavailable", Toast.LENGTH_SHORT)
+            toast.show()
+            println(ex.message)
+        }
         setContentView(R.layout.activity_main)
     }
 
     fun openFridge(view: View) {
         val intent = Intent(this, YourProductsActivity::class.java)
+
         startActivity(intent)
     }
 
@@ -36,4 +55,5 @@ class MainActivity : AppCompatActivity() {
         val basket = Intent(this, BasketActivity::class.java)
         startActivity(basket)
     }
+
 }
--- app/src/main/java/com/example/vlad/recipes/database/RecipesDatabaseHelper.kt
+++ app/src/main/java/com/example/vlad/recipes/database/RecipesDatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.vlad.recipes.database
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.DatabaseErrorHandler
 import android.database.sqlite.SQLiteDatabase
@@ -8,23 +9,39 @@ import android.database.sqlite.SQLiteOpenHelper
 class RecipesDatabaseHelper : SQLiteOpenHelper {
 
     constructor(context: Context) : super(context, DB_NAME, null, DB_VERSION) {
-        
+
     }
 
     constructor(context: Context, name: String, factory: SQLiteDatabase.CursorFactory, version: Int) : super(context, name, factory, version) {}
 
     constructor(context: Context, name: String, factory: SQLiteDatabase.CursorFactory, version: Int, errorHandler: DatabaseErrorHandler) : super(context, name, factory, version, errorHandler) {}
 
-    override fun onCreate(sqLiteDatabase: SQLiteDatabase) {
-
+    override fun onCreate(db: SQLiteDatabase) {
+        val sql = "CREATE TABLE products_types (\n" +
+                " _id integer PRIMARY KEY AUTOINCREMENT,\n" +
+                " name text NOT NULL\n" +
+                ");"
+        db.execSQL(sql)
+        insertProductType(db, "Vegetable")
     }
 
     override fun onUpgrade(sqLiteDatabase: SQLiteDatabase, i: Int, i1: Int) {
 
     }
 
+    fun insertProductType(db: SQLiteDatabase, name: String) {
+//        val productsTypesValues = ContentValues()
+//        productsTypesValues.put("_id", 1)
+//        productsTypesValues.put("name", name)
+//        db.insert("products_types", null, productsTypesValues)
+        val sql = "INSERT INTO products_types (\n" +
+                "  name)" +
+                "VALUES('" + name + "');"
+        db.execSQL(sql)
+    }
+
     companion object {
         private val DB_NAME = "recipes"
-        private val DB_VERSION = 1
+        private val DB_VERSION = 2
     }
 }
